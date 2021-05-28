3bfbc70ad3cca63c7fe0019d759b0bc2c6edd34e
etharm <eric.96@windowslive.com>
2021-05-28 10:07:02 +0200

    inital commit

---
+++ week5_exercise_skript.R
@@ -0,0 +1,10 @@
+library(readr)
+library(sf)
+library(terra)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(tmap)
+
+#---------------------------------------------------------------------------------------------------------
+
